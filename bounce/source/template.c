dbca7b787da76f8f21f4c9b6b6be6b2b889ff47e
Nathan Kessler <nathant93@gmail.com>
2018-09-06 08:16:15 -0400

    Remove unused code

--- bounce/source/template.c
+++ bounce/source/template.c
@@ -1,4 +1,13 @@
-#include <tonc_video.h>
+#include <tonc_types.h>
+
+#define CLR_BLACK   0x0000
+#define CLR_RED     0x001F
+#define CLR_LIME    0x03E0
+#define CLR_YELLOW  0x03FF
+#define CLR_BLUE    0x7C00
+#define CLR_MAG     0x7C1F
+#define CLR_CYAN    0x7FE0
+#define CLR_WHITE   0x7FFF
 
 #define REG_VCOUNT2 *(vu16*)0x04000006
 
