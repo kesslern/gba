d19880cda76d676181fb7e412e042106676da887
Nathan Kessler <nathant93@gmail.com>
2018-09-06 20:40:06 -0400

    More code cleanup

--- bounce/source/template.c
+++ bounce/source/template.c
@@ -9,24 +9,23 @@
 #define CLR_CYAN    0x7FE0
 #define CLR_WHITE   0x7FFF
 
-#define REG_VCOUNT2 *(vu16*)0x04000006
-
-void vid_vsync2()
+#define REG_VCOUNT *(vu16*) 0x04000006
+#define REG_DISPCNT *(u16*) 0x04000000
+#define PALETTE     ((u16*) 0x05000000)
+void vid_vsync()
 {
-  while(REG_VCOUNT2 >= 160);   // wait till VDraw
-  while(REG_VCOUNT2 < 160);    // wait till VBlank
+  while(REG_VCOUNT >= 160);   // wait till VDraw
+  while(REG_VCOUNT < 160);    // wait till VBlank
 }
 
 int main(void) {
-  u16* DISPCNT = (u16*) 0x04000000;
-  u16* PALETTE = (u16*) 0x05000000;
   u16* FRAME_1 = (u16*) 0x06000000;
   u16* FRAME_2 = (u16*) 0x0600A000;
 
   PALETTE[0] = CLR_BLUE;
   PALETTE[1] = CLR_RED;
   PALETTE[2] = CLR_YELLOW;
-  *DISPCNT = 0x0414;
+  REG_DISPCNT = 0x0414;
 
   for (int i = 0; i < 10; i++) {
     FRAME_1[10+i] = (u16) 0x0202;
@@ -36,11 +35,11 @@ int main(void) {
   }
 
   while(1) {
-    vid_vsync2();
-    vid_vsync2();
-    vid_vsync2();
+    vid_vsync();
+    vid_vsync();
+    vid_vsync();
 
-    *DISPCNT ^= 0x0010;
+    REG_DISPCNT ^= 0x0010;
   }
 
   return 0;
